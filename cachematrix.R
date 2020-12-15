46933642e5b23bd4737041e12f14fc1ca4d4ebff
HuiOng <angellaonghui@gmail.com>
2020-12-15 15:16:58 +0800

    Editted version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## A matrix of 3 row and 4 column is set, and the solved value,s is set as NULL
 
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix(c(1:12),3,4)) {
+    s <- NULL
+    set <- function(y) {
+      x <<- y
+      s <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) s <<- solve
+    getsolve <- function() s
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
+  }
+
+
+## Inversing the square matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inverse matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- mean(data, ...)
+  x$setsolve(s)
+  s
 }
